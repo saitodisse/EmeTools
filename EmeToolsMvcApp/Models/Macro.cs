4944848512e8c944c82eb054a03a1119c05a4457
saitodisse <saitodisse@gmail.com>
2010-08-25 02:08:16 +0000

    some new classes

---
+++ EmeToolsMvcApp/Models/Macro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmeToolsMvcApp.Models
+{
+    public class Macro
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual int Order { get; set; }
+        public virtual string Body { get; set; }
+        public virtual IList<Macro> MacrosFilho { get; set; }
+    }
+}
---
+++ EmeToolsMvcApp/Models/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmeToolsMvcApp.Models
+{
+    public class Menu
+    {
+        public virtual int Id { get; set; }
+        public virtual IList<Macro> MacrosPai { get; set; }
+    }
+}
