37e2972d55d687d732b71b9637760cd1826bdf70
saitodisse <saitodisse@gmail.com>
2012-01-18 22:00:45 -0200

    novos testes jsed regex. Não tem a opção de quantificadores não gulosos. :(

--- mvc3/MvcEmeTools/Scripts/EmeTools/emetoolsTest.js
+++ mvc3/MvcEmeTools/Scripts/EmeTools/emetoolsTest.js
@@ -43,35 +43,54 @@ $(document).ready(function () {
         equal(resultado, resultadoEsperado, "deve buscar a linha que possui def");
     });
 
-    test("SED: Rx: abc,   \\([[:alpha:]]\\)", function () {
-        deveCasar("abc", "\\([[:alpha:]]\\)");
+    test("SED: ^Rx:   abc,   [[:alpha:]]+", function () {
+        naoDeveCasar("abc", "[[:alpha:]]+"); //é necessário escapar o "+"
     });
-    test("SED: Rx: ABC,   \\([[:alpha:]]\\)", function () {
-        deveCasar("ABC", "\\([[:alpha:]]\\)");
+    test("SED: Rx: abc,   [[:alpha:]][[:alpha:]]*", function () {
+        deveCasar("abc", "[[:alpha:]][[:alpha:]]*"); //NÃO se deve escapar o "*"
     });
-    test("SED: ^Rx:   abc,   \\w\+", function () {
-        // infelizmente o SED não possui os apelidos para POSIX
-        naoDeveCasar("abc", "\\w\+");
+    test("SED: ^Rx:   abc,   [[:alpha:]][[:alpha:]]\\*", function () {
+        naoDeveCasar("abc", "[[:alpha:]][[:alpha:]]\\*"); //NÃO se pode escapar o "*"
     });
-    test("SED: ^Rx:   áéí,   \\([[:alpha:]]\\)", function () {
-        // a classe POSIX não casa acentos, que pena
-        naoDeveCasar("áéí", "\\([[:alpha:]]\\)");
+    test("SED: Rx: abc,   [[:alpha:]]\\+", function () {
+        deveCasar("abc", "[[:alpha:]]\\+");
+    });
+    test("SED: Rx: ABC,   [[:alpha:]]\\+", function () {
+        deveCasar("ABC", "[[:alpha:]]\\+");
+    });
+    test("SED: ^Rx:   abc,   \\w\\+", function () {
+        naoDeveCasar("abc", "\\w\\+"); // infelizmente o SED não possui os apelidos para POSIX
+    });
+    test("SED: ^Rx:   áéí,   [[:alpha:]]\\+", function () {
+        naoDeveCasar("áéí", "[[:alpha:]]\\+"); // a classe POSIX não casa acentos, que pena
+    });
+    test("SED: Rx: áéí,   [a-zàáâãéêíóôõúüç]\\+", function () {
+        deveCasar("áéí", "[a-zàáâãéêíóôõúüç]\\+"); // é necessário informar manualmente
+    });
+    test("SED: ^Rx:   AAA,   [a]\\+", function () {
+        naoDeveCasar("AAA", "[a]\\+"); // é case sensitive e NÃO tem o modificador ignore case (já testei)
+    });
+    test("SED: Rx: áÉÍ,   [a-zàáâãéêíóôõúüçA-ZÀÁÂÃÉÊÍÓÔÕÚÜÇ]\\+", function () {
+        deveCasar("áÉÍ", "[a-zàáâãéêíóôõúüçA-ZÀÁÂÃÉÊÍÓÔÕÚÜÇ]\\+"); // fica grande, mas é o jeito
+    });
+    test("SED: Rx: áÉÍ12_EF,   [0-9_a-zàáâãéêíóôõúüçA-ZÀÁÂÃÉÊÍÓÔÕÚÜÇ]\\+", function () {
+        deveCasar("áÉÍ12_EF", "[0-9_a-zàáâãéêíóôõúüçA-ZÀÁÂÃÉÊÍÓÔÕÚÜÇ]\\+"); // emulando o \w
     });
 
     var deveCasar = function (texto, regex) {
         var textoComNewLine = texto + "\n";
-        var sedScript = "s/" + regex + "/\\1/gp" + "\n";
+        var sedScript = "s/^\\(" + regex + "\\)$/\\1/p" + "\n";
         var resultadoEsperado = textoComNewLine;
         var resultado = sedJsed(textoComNewLine, sedScript, true, false, 10000);
-        equal(resultado, resultadoEsperado, "a regex [" + regex + "] não casou com[" + texto + "]");
+        equal(resultado, resultadoEsperado, "a regex " + regex + " não casou com [" + texto + "]");
     };
 
     var naoDeveCasar = function (texto, regex) {
         var textoComNewLine = texto + "\n";
-        var sedScript = "s/" + regex + "/\\1/gp" + "\n";
+        var sedScript = "s/^\\(" + regex + "\\)$/\\1/p" + "\n";
         var resultadoEsperado = textoComNewLine;
         var resultado = sedJsed(textoComNewLine, sedScript, true, false, 10000);
-        notEqual(resultado, resultadoEsperado, "a regex [" + regex + "] não deveria casar com[" + texto + "]");
+        notEqual(resultado, resultadoEsperado, "a regex " + regex + " não deveria casar com " + texto + "]");
     };
 
     test("x[xx] sem newline", function () {
