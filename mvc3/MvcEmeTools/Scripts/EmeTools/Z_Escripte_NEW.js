9321306eda7edf9115170ff3b7482730c7ff2b2e
saitodisse <saitodisse@gmail.com>
2012-03-10 21:43:47 -0300

    novos scripts para fazer atalho no EmEditor

---
+++ mvc3/MvcEmeTools/Scripts/EmeTools/Z_Escripte_NEW.js
@@ -0,0 +1,47 @@
+#include "../jsed/jsed.js"
+#include "emetools.js"
+#include "comandos.js"
+#include "roboXixi.js"
+#include "transformarXxx.js"
+#include "xixizero.js"
+
+var NEW_LINE = "\n";
+
+SHOW_ALERT_ON_ERRORS = true;
+
+// HELPERS FUNCTIONS
+var getAllText = function () {
+    document.selection.SelectAll();
+    var text = document.selection.text;
+
+    document.HighlightFind = false;
+    document.selection.EndOfLine(false, eeLineView);
+    return text;
+};
+var sameWindow = function (texto) {
+    document.selection.SelectAll();
+    document.selection.text = texto;
+
+    document.HighlightFind = false;
+    document.selection.EndOfLine(false, eeLineView);
+};
+var newEditorWindow = function (texto) {
+    editor.NewFile();
+    document.write(texto);
+
+    document.HighlightFind = false;
+    document.selection.EndOfLine(false, eeLineView);
+};
+// (END) HELPERS FUNCTIONS
+
+// take all text from windows
+var allText = getAllText();
+
+// initialize roboXixizero
+var roboXixizero = new RoboXixi(allText, NEW_LINE);
+
+// make the transformation
+roboXixizero.transformar();
+
+// print result in a new window
+newEditorWindow(roboXixizero.ResultadoFinal);
